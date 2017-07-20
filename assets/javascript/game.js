fc2a525320da429960a7aa589dea8ed8f50ce958
fodnb <Daniel.Mcghee6813@gmail.com>
2017-07-20 08:40:21 -0400

    on click now working in firefox

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // here we make sure that the page fully loads before we perform any actions
 $(document).ready(function() {
 
@@ -9,13 +11,9 @@ $(document).ready(function() {
     var wins = 0;
     var losses = 0;
     var buttonImg = ["assets/images/crystal1.png", "assets/images/crystal2.png", "assets/images/crystal3.png", "assets/images/crystal4.png"];
-    console.log(numOptions);
-    console.log(buttonImg.length);
-    console.log(buttonImg);
-
     var random = getRandomInteger(19, 121);
-    console.log(random);
-    console.log(typeof random);
+
+
 
     // for when page loads all random #'s are on the page 
     $("#answer").html(random);
@@ -31,7 +29,7 @@ $(document).ready(function() {
 
     // here we're pushing 4 numbers into our array to distribute to the images later
     function randomArray() {
-        for (i = 0; i < 4; i++) {
+        for (var i = 0; i < 4; i++) {
             numOptions[i] = getRandomInteger(1, 13);
         }
     }
@@ -43,7 +41,7 @@ $(document).ready(function() {
     // new random #
     function startGame() {
         $("#buttons").empty();
-       
+
 
         numOptions = [];
         console.log(numOptions);
@@ -57,12 +55,7 @@ $(document).ready(function() {
         makeButtons();
         clicking();
         $("#pScore").html("Your total score is: " + counter);
-        // document.getElementById("pScore").innerHTML = "Your total score is: " + counter;
         $("#answer").html("# " + random);
-        // document.getElementById("answer").innerHTML = random;
-
-
-
     }
 
 
@@ -72,62 +65,59 @@ $(document).ready(function() {
     // attaching a value to each from numOptions array to give each a value
 
 
-function makeButtons(){
-    for (var i = 0; i < numOptions.length; i++) {
-        var newButton = $("<button>");
-        newButton.attr("id", "mybutton" + [i]);
-        $("mybutton").attr("class", "crystals");
-        newButton.addClass("crystals");
-        $("#buttons").append(newButton);
+    function makeButtons() {
+        for (var i = 0; i < numOptions.length; i++) {
+            // var newButton = $("<button>");
+            // newButton.attr("id", "mybutton" + [i]);
+            // $("mybutton").attr("class", "crystals");
+            // newButton.addClass("crystals");
+            // $("#buttons").append(newButton);
 
-        var newImage = $("<img>");
-        newImage.attr("id", "myImg");
-        $("newImage").attr("class", "images");
-        newImage.addClass("images");
-        newImage.attr("src", buttonImg[i]);
-        newImage.attr("data-cValue", numOptions[i]);
-        $("#mybutton" + [i]).append(newImage);
+            var newImage = $("<img>");
+            newImage.attr("id", "myImg");
+            $("newImage").attr("class", "images");
+            newImage.addClass("images");
+            newImage.attr("src", buttonImg[i]);
+            newImage.attr("data-cValue", numOptions[i]);
+            $("#buttons").append(newImage);
+        }
     }
-}
 
-// makeButtons();
-startGame();
+    startGame();
 
     // if user clicks on an image game will take the value from that button and makes it a number.
     // the game then adds that number to the current count
-  
-function clicking(){
-    $("img.images").on("click", function() {
-         $(this).animate({ opacity: "0.75" });
-         $(this).animate({ opacity: "0.95" });
-         $(this).animate({ opacity: "0.75" });
-         $(this).animate({ opacity: "1" });
-        var cValue = ($(this).attr("data-cValue"));
-        var cValue = parseInt(cValue);
-        counter += cValue;
-        console.log(counter);
-        $("#pScore").html("Your total score is: " + counter);
-       
 
-        // here we just have our simple game rules.  Win is if the random # equals the counter
-        // a loss is if we go over
-        // we restart the game in either event we add a win or loss to those totals
-        if (counter === random) {
-            wins++;
-            console.log(wins);
-            $("h4#wins").html("Wins: " + wins);
-            // document.getElementById("wins").innerHTML = "Wins: " + wins;
-            startGame();
-        } else if (counter > random) {
-            losses++;
-            console.log(losses);
-            $("h4#losses").html("Losses: " + losses);
-            // document.getElementById("losses").innerHTML = "Losses: " + losses;
-            startGame();
-        }
+    function clicking() {
+        $("img.images").on("click", function(event){
+            console.log("i'm clicked");
+            $(this).animate({ opacity: "0.75" });
+            $(this).animate({ opacity: "0.95" });
+            $(this).animate({ opacity: "0.75" });
+            $(this).animate({ opacity: "1" });
+            var cValue = parseInt($(this).attr("data-cValue"));
+            counter += cValue;
+            console.log(counter);
+            $("#pScore").html("Your total score is: " + counter);
 
- });
-}   
+
+            // here we just have our simple game rules.  Win is if the random # equals the counter
+            // a loss is if we go over
+            // we restart the game in either event we add a win or loss to those totals
+            if (counter === random) {
+                wins++;
+                console.log(wins);
+                $("h4#wins").html("Wins: " + wins);
+                startGame();
+            } else if (counter > random) {
+                losses++;
+                console.log(losses);
+                $("h4#losses").html("Losses: " + losses);
+                startGame();
+            }
+
+        });
+    }
 
 
 
